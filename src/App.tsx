b348749d3974e930fadab27584e41bf40e661372
Jon Mico <jonmico@gmail.com>
2023-06-29 19:45:38 -0400

    Add shadows, remove redudant wrapper div.

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,38 @@ import { styled } from 'styled-components';
 // hex code for the e on linux is ctrl + shift + u 00E9
 
 const MainWrapper = styled.div`
-  width: 60%;
-  min-width: 500px;
-  margin: auto;
-`;
-
-const Header = styled.h1`
-  font-family: Silkscreen;
-  font-size: 4rem;
-  font-weight: 700;
-  text-align: center;
-`;
-
-const PokedexWrapper = styled.div`
+  width: 75%;
+  min-width: 1000px;
+  margin: 5rem auto;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const Header = styled.h1`
+  font-family: Silkscreen;
+  font-size: 3rem;
+  font-weight: 700;
+  margin: 1rem auto;
+  text-align: center;
+`;
+
 const PokedexHalf = styled.div`
   height: 700px;
-  width: 800px;
-  background-color: red;
+  width: 500px;
+  background-color: #cc322a;
   margin: 0 5px 0 5px;
-  overflow: auto;
+  border-radius: 15px;
+  box-shadow: 1px 1px 5px black, -1px -1px 5px black;
 `;
 
 export default function App() {
   return (
     <MainWrapper>
-      <Header>Pokédex</Header>
-      <PokedexWrapper>
-        <PokedexHalf></PokedexHalf>
-        <PokedexHalf></PokedexHalf>
-      </PokedexWrapper>
+      <PokedexHalf>
+        <Header>Pokédex</Header>
+      </PokedexHalf>
+      <PokedexHalf></PokedexHalf>
     </MainWrapper>
   );
 }
