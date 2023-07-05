a9e90caf7d441022cb438cc496f0602a547456f4
Jon Mico <jonmico@gmail.com>
2023-07-04 23:42:33 -0400

    Remove some imports.

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { PokemonCallItem, PokemonDataType } from '../types';
 import PokemonHeader from './PokemonHeader';
